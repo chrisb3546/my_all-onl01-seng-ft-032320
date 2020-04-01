db50c165a47ec1ac8e592f37bebdc575a8b2137e
chris brady <chrisb3546@yahoo.com>
2020-04-01 16:41:23 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,9 +3,10 @@ require 'pry'
 def my_all?(collection)
   i = 0
   while i < collection.length
-   yield (collection[i])
+  return false if !yield (collection[i])
    i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
   end
+  true
 end
 
 my_all?(collection){|i| i < 3}
